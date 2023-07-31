71310cdf3b9d8f983598eabcc8aa2a9a7dd242ac
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-07-31 16:10:43 +0300

    Third commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,22 @@
 fun main() {
     var price = 1_000_000
-    val purch1 = 1_000
-    val purch2 = 1_001
-    val purch3 = 10_000
-    val purch4 = 10_001
+    val purch1 = 1_001
+    val purch2 = 10_000
+    val purch3 = 10_001
     val discMin = 100
     val discMax = 5
     val discAdd = 1
     val userLevel = "REGULAR_CUSTOMER"
 
-    if (price <= purch1) price
-    if (price >= purch2 && price <= purch3) price -= discMin
+    if (price >= purch1 && price <= purch2) price -= discMin
 
     val disc5 = price * discMax / 100
-    if (price >= purch4) price -= disc5
+    if (price >= purch3) price -= disc5
 
     var userDiscount = when (userLevel) {
         "REGULAR_CUSTOMER" -> discAdd
-        else -> price
+        else -> 0
     }
-
     val disc6 = price * userDiscount / 100
     price -= disc6
 
