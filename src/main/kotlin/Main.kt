595b3676c70a39f5216b59a44c7a7a51fb6eff9f
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-07-31 11:58:40 +0300

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,27 @@
 fun main() {
-    val price = 8000
-    val discountMin = 100
-    val discountMax = 0.05
-    val addDiscount = 0.01
+    var price = 1_000_000
+    val purch1 = 1_000
+    val purch2 = 1_001
+    val purch3 = 10_000
+    val purch4 = 10_001
+    val discMin = 100
+    val discMax = 5
+    val discAdd = 1
+    val userLevel = "REGULAR_CUSTOMER"
 
-    val totalDiscountMax = price * discountMax
-    val purchase = if (price < 1000) price else if (price > 1001 && price < 10000) discountMin else totalDiscountMax
-    val total = price - purchase.toInt()
-    val total2 = total * addDiscount
-    val total3 = total - total2
+    if (price <= purch1) price
+    if (price >= purch2 && price <= purch3) price -= discMin
+
+    val disc5 = price * discMax / 100
+    if (price >= purch4) price -= disc5
+
+    var userDiscount = when (userLevel) {
+        "REGULAR_CUSTOMER" -> discAdd
+        else -> price
+    }
+
+    val disc6 = price * userDiscount / 100
+    price -= disc6
 
     println("Сумма покупки $price руб.")
-    println("Сумма покупки со скидкой $total руб.")
-    println("Сумма покупки постоянного пользователя с доп. скидкой $total3 руб.")
 }
